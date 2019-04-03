9ade54a1038d868173edca3ceb4e0155c081317e
Lee Tuan <tuanlm1989@gmail.com>
2019-04-03 09:47:23 +0700

    Update

--- crawl.sh
+++ crawl.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 working_dir=`pwd`
 function crawl(){
-	wget -qO- ${1} | awk -f ${working_dir}/parse.awk | while read name address phone; do
+	wget -qO- ${1} | awk -f ${working_dir}/parse_vtown_company.awk | while read name address phone; do
 		echo ${name}
 		echo ${address}
 		echo ${phone}
 	done
 }
-crawl http://vtown.vn/company/hoang-ha-office-stationery-co-ltd.html
+while read vtownUrl; do
+	crawl ${vtownUrl}
+done < "listcompany_url.txt"
